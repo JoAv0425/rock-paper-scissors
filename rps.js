de4c052b9551c95b8ffe4f9db685783048283186
JoAv0425 <praisecandomo@gmail.com>
2026-02-10 15:43:25 +0100

    Write logic to get computer choice

--- rps.js
+++ rps.js
@@ -1,1 +1,14 @@
 // Game code. Let's go
+console.log("Hello World")
+function getComputerChoice(opponent) {
+    opponent = (Math.random() * 10)
+    if (opponent <= 3.3) {
+        return "rock";
+        } else if (opponent > 3.3 && opponent <= 6.6) {
+            return "paper";
+    } else return "scissors";
+}
+console.log(getComputerChoice())
+
+
+
